2bd741a35c2fc7f0d891b84cd7668f5ba61dd996
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2025-06-20 18:20:54 +0530

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -51,13 +51,13 @@ const Product = ({product}) => {
   return (
     <>
     
-    <div className='px-4'>
-      <div className='h-[60vh] w-[20vw] rounded-[10px] overflow-hidden bg-white '>
-        <div className='p-5 bg-white rounded-2xl flex items-center justify-center ' >
+    <div className='px-0 mx-1 sm:mx-0 sm:px-0'>
+      <div className='h-[35vh] sm:h-[60vh] hidden sm:block w-[20vw] rounded-[10px] overflow-hidden bg-white '>
+        <div className='p-1 sm:p-5 bg-white rounded-2xl flex items-center justify-center ' >
         <Link to={`/product/${id}`}>
           <FaEye className="text-2xl cursor-pointer z-50 hover:text-blue-600 transition-colors duration-300" />
         </Link>
-            <img className='rounded-2xl p-10 h-[39vh] group-hover:scale-110 transition duration-300 border border-[#e4e4e4]' src={image} alt="" />
+            <img className='rounded-2xl p-10 h-[17vh] sm:h-[39vh] group-hover:scale-110 transition duration-300 border border-[#e4e4e4]' src={image} alt="" />
         </div>
         <div className='p-10 pt-0 flex row space-x-7'>
             <div className='' >{displayTitle}</div>
@@ -65,6 +65,30 @@ const Product = ({product}) => {
         </div>
         <div className='h-[46px] w-[190px] overflow-hidden rounded-4xl ml-13 relative bottom-[35px]'>
         <button className='relative object-cover scale-[1.9] bottom-[20px] p-[0px] cursor-pointer' onClick={handleClick}>
+      <Lottie
+      loop={false}
+      autoplay={false}
+      lottieRef={lottieRef}
+      animationData={Btn}/></button>
+          
+          
+        </div>
+      </div>
+
+      {/* Phone  */}
+      <div className='h-[35vh] sm:hidden w-[38vw] rounded-[10px] flex flex-col items-center overflow-hidden p-3 bg-white '>
+        <div className='p-1 bg-white rounded-2xl h-[17vh] flex items-center justify-center ' >
+        <Link to={`/product/${id}`}>
+          <FaEye className="text-2xl cursor-pointer absolute top-0 z-50 hover:text-blue-600 transition-colors duration-300" />
+        </Link>
+            <img className='rounded-2xl p-7 group-hover:scale-110 transition duration-300 border border-[#e4e4e4]' src={image} alt="" />
+        </div>
+        <div className=' pt-0 flex absolute bottom-[50px] flex-col'>
+            <div className='bg-black text-white absolute bottom-[64px] left-0 whitespace-pre'> ₹{Math.round(price * 83)} </div>
+            <div className='w-[27vw] p-1 bottom-1 relative flex justify-center items-center' >{displayTitle}</div>
+        </div>
+        <div className='h-[26px] w-[150px] overflow-hidden rounded-4xl relative left-4 top-[92px]'>
+        <button className='relative object-cover scale-[1.5] bottom-[25px] right-[16px] p-[0px] cursor-pointer' onClick={handleClick}>
       <Lottie
       loop={false}
       autoplay={false}
