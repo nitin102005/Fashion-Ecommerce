849033305a8001aac685bcdc262c35b5721625ce
Nitin Singh Pokhariya <168846806+nitin102005@users.noreply.github.com>
2025-06-21 18:57:06 +0530

    Update Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -129,16 +129,16 @@ function Section() {
           <button type="button" className='bg-white z-30 pointer-events-auto w-[393px] relative flex items-center justify-center cursor-pointer' onClick={()=>{
             document.getElementById("mennav")?.scrollIntoView({behavior: "smooth"})
           }}>SHOP NOW</button>
-          <button className='bg-black text-[#FADADD] flex items-center justify-center  relative left-[13px]  w-[388px] cursor-pointer' onClick={()=>{
+          <button className='bg-black text-[#FADADD] flex items-center justify-center  relative left-[8px]  w-[388px] cursor-pointer' onClick={()=>{
             document.getElementById("womennav")?.scrollIntoView({behavior: "smooth"})
           }}>SHOP NOW</button>
-          <button className='bg-[#D4AF37] text-[#1C1C1C] flex items-center justify-center relative left-[29px] w-[390px] cursor-pointer' onClick={()=>{
+          <button className='bg-[#D4AF37] text-[#1C1C1C] flex items-center justify-center relative left-[17px] w-[390px] cursor-pointer' onClick={()=>{
             document.getElementById("anav")?.scrollIntoView({behavior: "smooth"})
           }}>SHOP NOW</button>
-          <button className='bg-[#1A1A1A] text-[#ECE6DA] flex items-center justify-center relative left-[40px] w-[389px] cursor-pointer' onClick={()=>{
+          <button className='bg-[#1A1A1A] text-[#ECE6DA] flex items-center justify-center relative left-[27px] w-[389px] cursor-pointer' onClick={()=>{
             document.getElementById("snav")?.scrollIntoView({behavior: "smooth"})
           }}>SHOP NOW</button>
-          <button className='bg-[#3E1F47] text-[#EADCF8] flex items-center justify-center relative left-[58px] w-[383px] cursor-pointer' onClick={()=>{
+          <button className='bg-[#3E1F47] text-[#EADCF8] flex items-center justify-center relative left-[39px] w-[383px] cursor-pointer' onClick={()=>{
             document.getElementById("wnav")?.scrollIntoView({behavior: "smooth"})
           }}>SHOP NOW</button>
         </div>
